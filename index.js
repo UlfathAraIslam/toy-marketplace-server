2bcd12204fd1f6a397855670f85728c3d5c22154
Ulfath Ara Islam <mouliajsahid@gmail.com>
2023-05-21 22:09:11 +0900

    await is deleted from await client.connet

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -25,7 +25,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+     client.connect();
 
     const toyCollection = client.db('toyMarket').collection('toys');
 
@@ -35,6 +35,12 @@ async function run() {
       res.send(result);
     })
 
+    app.get('/toys/:id', async(req,res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await toyCollection.findOne(query);
+      res.send(result)
+    })
 
 
 
